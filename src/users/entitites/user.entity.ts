512400884d201fba602983c57eb1beb7dadd7a03
mario mora <jorgemariomoramolina@gmail.com>
2026-01-12 21:03:22 -0500

    relations in nestjs

--- src/users/entitites/user.entity.ts
+++ src/users/entitites/user.entity.ts
@@ -1,4 +1,6 @@
-import { Column, Entity, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, JoinColumn, OneToOne } from 'typeorm';
+
+import { Profile } from '../entitites/profile.entity';
 
 @Entity({
   name: 'users',
@@ -18,4 +20,8 @@ export class User {
 
   @UpdateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP', name: 'updated_at' })
   updatedAt: Date;
+
+  @OneToOne(() => Profile, { nullable: false, cascade: true })
+  @JoinColumn({ name: 'profile_id' })
+  profile: Profile;
 }
