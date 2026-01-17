043c4abc1472884d665855d7425bdb27cbb4bd3f
mario mora <jorgemariomoramolina@gmail.com>
2026-01-17 10:26:37 -0500

    categories module

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
-import { PostsService } from './posts.service';
-import { PostsController } from './posts.controller';
+import { PostsService } from '../posts/services/posts.service';
+import { PostsController } from '../posts/controllers/posts.controller';
+import { CategoriesService } from '../posts/services/categories.service';
+import { CategoriesController } from '../posts/controllers/categories.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from './entities/post.entity';
+import { Category } from './entities/category.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Post])],
-  controllers: [PostsController],
-  providers: [PostsService],
-  exports: [PostsService],
+  imports: [TypeOrmModule.forFeature([Post, Category])],
+  controllers: [PostsController, CategoriesController],
+  providers: [PostsService, CategoriesService],
+  exports: [PostsService, CategoriesService],
 })
 export class PostsModule {}
