aa35952e2e70ae5238eb3cdc69c8d9788cb7e976
mario mora <jorgemariomoramolina@gmail.com>
2026-01-19 01:45:54 -0500

    login endpoint

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,11 @@ import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './services/auth.service';
 import { LocalStrategy } from '../auth/strategies/local.strategy';
 import { UsersModule } from '../users/users.module';
+import { AuthController } from '../auth/controllers/auth.controller';
 
 @Module({
   imports: [UsersModule, PassportModule],
   providers: [AuthService, LocalStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
